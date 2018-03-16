ab9197f7e9c8fe6b03ef4f3f1da03d9f776f887e
Sebastian Śliwiński <sxsliwinski@gmail.com>
2018-03-16 21:25:23 +0100

    add scroll

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $(document).on('click', 'a[href^="#"]', function (event){
         event.preventDefault();
         
-        $('body').animate({
+        $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 1000);
     });
